f3fb385c8c312ec1a34edf5908cf5b7c57edb371
Oliver Holmes <oliver.albert.holmes@gmail.com>
2022-11-05 17:20:48 +1030

    Update file structure to root

--- server/index.js
+++ server/index.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
-
-
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
