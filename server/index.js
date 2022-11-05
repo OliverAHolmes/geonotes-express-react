cd2e9b8f3eae9a68110f788adf5b1987f8521d36
Oliver Holmes <oliver.albert.holmes@gmail.com>
2022-11-05 14:10:17 +1030

    Add GeoJSON api connection

--- server/index.js
+++ server/index.js
@@ -6,11 +6,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+
+
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api/:dataset", (req, res) => {
+    const geoJSON = require('./GeoJSON/'+req.params.dataset+'.json')
+    res.json(geoJSON);
 });
 
 app.listen(PORT, () => {
