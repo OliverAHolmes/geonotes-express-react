2d9d380419244e6f6716680900caedf434adb09e
Oliver Holmes <oliver.albert.holmes@gmail.com>
2022-11-06 09:34:03 +1030

    Remove unused comments

--- client/src/App.js
+++ client/src/App.js
@@ -41,9 +41,9 @@ const dataSetsUrls = {
 
 const App = () => {
 
-  const [results, setResults] = React.useState({});
+  const [results, setResults] = useState({});
 
-  React.useEffect(() => {
+  useEffect(() => {
 
     axios.all(dataSetsUrls.requests.map((endpoint) => axios.get(endpoint)))
       .then(axios.spread((...responses) => {
@@ -151,29 +151,3 @@ return (
   );
 }
 export default App;
-// // client/src/App.js
-
-// import React from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// function App() {
-//   const [data, setData] = React.useState(null);
-
-//   React.useEffect(() => {
-//     fetch("/api")
-//       .then((res) => res.json())
-//       .then((data) => setData(data.message));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>{!data ? "Loading..." : data}</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
